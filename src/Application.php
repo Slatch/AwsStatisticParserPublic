ebc8ef511799c8c3257fd8d686d19dc5603d3ebc
Leonov <slatch@yandex.ru>
2024-05-15 13:14:12 +0200

    [CORE-7593] [Pdffiller] Analyze "S3 Inventory" logs

--- src/Application.php
+++ src/Application.php
@@ -6,30 +6,33 @@ use Aws\Api\DateTimeResult;
 use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
 use Aws\Sts\StsClient;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Tools\SchemaTool;
-use FSStats\Db\EntityManagerBuilder;
-use FSStats\Db\Orm\LastProceeded;
-use FSStats\Db\Orm\Stats;
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\ClientBuilder;
 use GuzzleHttp\Psr7\Stream;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class Application
 {
-    private S3Client $client;
+    private S3Client $s3client;
+    private Client $elasticClient;
     private OutputInterface $output;
-    private EntityManager $entityManager;
     private DateTimeResult $tokenExpiration;
     private const TOKEN_EXPIRATION_THRESHOLD = 20; // in minutes
 
     public function __construct()
     {
+        $this->elasticClient = ClientBuilder::create()
+            ->setHosts([
+                $_ENV['ELASTICSEARCH_HOST']
+            ])
+            // https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/connecting.html
+            ->setElasticCloudId($_ENV['ELASTICSEARCH_CLOUD_ID'] ?? '')
+            ->setApiKey($_ENV['ELASTICSEARCH_API_KEY'] ?? '')
+            ->build();
         $this->output = new ConsoleOutput();
-        $this->entityManager = (new EntityManagerBuilder())->get();
 
-        $this->initClient();
-        $this->initDb();
+        $this->initS3Client();
     }
 
     public function run(): void
@@ -44,7 +47,7 @@ final class Application
         foreach ($dates as $date) {
             $this->output->writeln('[' . $date . '] Get symlink.txt');
             try {
-                $result = $this->client->getObject([
+                $result = $this->s3client->getObject([
                     'Bucket' => $_ENV['BUCKET_NAME_READ'],
                     'Key' => $_ENV['STATISTIC_FOLDER_PATH'] . 'hive/dt=' . $date . '-01-00/symlink.txt',
                 ]);
@@ -62,22 +65,11 @@ final class Application
 
             $body->close();
 
-            $this->output->writeln(sprintf('[%s] Filter links:', $date));
-            $gzipUrls = $this->filterGzips($gzipUrls);
-            if (empty($gzipUrls)) {
-                $this->output->writeln(sprintf(' > Skipped %s', $date));
-                continue;
-            }
-
             $this->output->writeln('[' . $date . '] Process links');
             $this->processGzips($gzipUrls);
         }
 
         $this->output->writeln('Done');
-
-        while ($_ENV['TERMINATE'] ?? true) {
-            sleep(1);
-        }
     }
 
     private function isNeededToUpdateToken(): bool
@@ -91,7 +83,7 @@ final class Application
             $interval->i <= self::TOKEN_EXPIRATION_THRESHOLD;
     }
 
-    private function initClient(): void
+    private function initS3Client(): void
     {
         $stsClient = new StsClient([
             'region' => $_ENV['REGION_READ'],
@@ -111,7 +103,7 @@ final class Application
         $this->output->writeln('----------');
         $this->output->writeln('');
 
-        $this->client = new S3Client([
+        $this->s3client = new S3Client([
             'version' => 'latest',
             'region' => $_ENV['REGION_READ'],
             'credentials' =>  [
@@ -120,28 +112,7 @@ final class Application
                 'token' => $result['Credentials']['SessionToken']
             ]
         ]);
-        $this->client->registerStreamWrapperV2();
-    }
-
-    private function initDb(): void
-    {
-        $tool = new SchemaTool($this->entityManager);
-        $classes = [
-            $this->entityManager->getClassMetadata(Stats::class),
-            $this->entityManager->getClassMetadata(LastProceeded::class),
-        ];
-        $tool->dropSchema($classes);
-        $tool->createSchema($classes);
-    }
-
-    private function filterGzips(array $gzipUrls): array
-    {
-        /** @var string[] $proccededGzips */
-        $proccededGzips = array_map(function(LastProceeded $lastProceeded) {
-            return $lastProceeded->getGzip();
-        }, $this->entityManager->getRepository(LastProceeded::class)->findAll());
-
-        return array_diff($gzipUrls, $proccededGzips);
+        $this->s3client->registerStreamWrapperV2();
     }
 
     private function processGzips(array $gzipUrls): void
@@ -150,7 +121,7 @@ final class Application
         $this->output->write('Total: ' . $max . ' | Processed: ');
         foreach ($gzipUrls as $index => $gzipUrl) {
             if ($this->isNeededToUpdateToken()) {
-                $this->initClient();
+                $this->initS3Client();
             }
             $this->processGzipUrl($gzipUrl);
             $this->output->write(($index + 1) . ' ');
@@ -164,7 +135,7 @@ final class Application
         $attempt = 0;
         do {
             if (($stream = fopen($gzipUrl, 'r')) === false) {
-                $this->initClient();
+                $this->initS3Client();
                 $this->output->writeln('Failed to open stream. Retry...');
             }
         } while (++$attempt < 3);
@@ -173,41 +144,23 @@ final class Application
             'window' => 32,
         ]);
 
-        $counter = 0;
         while (
             ($data = fgetcsv($stream, 1000)) !== false
         ) {
             // Bucket, Key, VersionId, IsLatest, IsDeleteMarker, Size
-            /*if (strtolower($data[4]) !== 'false') { // isDeleteMarker
-                continue;
-            }
-            if (strtolower($data[3]) !== 'true') { // isLatest
-                continue;
-            }*/
 
-            $stat = new Stats();
-            $stat->setKey($data[1]);
-            $stat->setVersion($data[2]);
-            $stat->setIsLatest($data[3] == 'true');
-            $stat->setIsDeleteMarker($data[4] == 'true');
-            if (!empty($data[5]) || $data[5] === '0') {
-                $stat->setSize((int)$data[5]);
-            }
-
-            $this->entityManager->persist($stat);
-
-            if ((++$counter % $_ENV['BATCH_SIZE']) === 0) {
-                $this->entityManager->flush();
-                $this->entityManager->clear();
-            }
+            $this->elasticClient->index([
+                'index' => $_ENV['INDEX_NAME'],
+                'id' => 'fs-stats-' . time() . '-' . uniqid() . '-' . rand(0, 9999),
+                'body' => [
+                    'key' => $data[1],
+                    'version' => $data[2],
+                    'isLatest' => $data[3] == 'true',
+                    'isDeleteMarker' => $data[4] == 'true',
+                    'size' => $data[5] === '' ? 0 : (int)$data[5],
+                ],
+            ]);
         }
         fclose($stream);
-
-        $lastProceeded = new LastProceeded();
-        $lastProceeded->setGzip($gzipUrl);
-        $this->entityManager->persist($lastProceeded);
-
-        $this->entityManager->flush();
-        $this->entityManager->clear();
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,14 +9,14 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
-        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
-        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
-        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
     );
 
@@ -29,11 +29,8 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
             'Symfony\\Polyfill\\Intl\\Grapheme\\' => 31,
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Contracts\\Service\\' => 26,
-            'Symfony\\Contracts\\Cache\\' => 24,
-            'Symfony\\Component\\VarExporter\\' => 30,
             'Symfony\\Component\\String\\' => 25,
             'Symfony\\Component\\Console\\' => 26,
-            'Symfony\\Component\\Cache\\' => 24,
         ),
         'P' => 
         array (
@@ -41,12 +38,17 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Http\\Client\\' => 16,
             'Psr\\Container\\' => 14,
-            'Psr\\Cache\\' => 10,
             'PhpOption\\' => 10,
         ),
         'J' => 
         array (
             'JmesPath\\' => 9,
+        ),
+        'H' => 
+        array (
+            'Http\\Promise\\' => 13,
+            'Http\\Discovery\\' => 15,
+            'Http\\Client\\' => 12,
         ),
         'G' => 
         array (
@@ -59,18 +61,14 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         array (
             'FSStats\\' => 8,
         ),
+        'E' => 
+        array (
+            'Elastic\\Transport\\' => 18,
+            'Elastic\\Elasticsearch\\' => 22,
+        ),
         'D' => 
         array (
             'Dotenv\\' => 7,
-            'Doctrine\\Persistence\\' => 21,
-            'Doctrine\\ORM\\' => 13,
-            'Doctrine\\Instantiator\\' => 22,
-            'Doctrine\\Inflector\\' => 19,
-            'Doctrine\\Deprecations\\' => 22,
-            'Doctrine\\DBAL\\' => 14,
-            'Doctrine\\Common\\Lexer\\' => 22,
-            'Doctrine\\Common\\Collections\\' => 28,
-            'Doctrine\\Common\\' => 16,
         ),
         'A' => 
         array (
@@ -103,14 +101,6 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         array (
             0 => __DIR__ . '/..' . '/symfony/service-contracts',
         ),
-        'Symfony\\Contracts\\Cache\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/cache-contracts',
-        ),
-        'Symfony\\Component\\VarExporter\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/var-exporter',
-        ),
         'Symfony\\Component\\String\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/string',
@@ -118,10 +108,6 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         'Symfony\\Component\\Console\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/console',
-        ),
-        'Symfony\\Component\\Cache\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/cache',
         ),
         'Psr\\Log\\' => 
         array (
@@ -140,10 +126,6 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
-        'Psr\\Cache\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/cache/src',
-        ),
         'PhpOption\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
@@ -151,6 +133,18 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         'JmesPath\\' => 
         array (
             0 => __DIR__ . '/..' . '/mtdowling/jmespath.php/src',
+        ),
+        'Http\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/promise/src',
+        ),
+        'Http\\Discovery\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/discovery/src',
+        ),
+        'Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/httplug/src',
         ),
         'GuzzleHttp\\Psr7\\' => 
         array (
@@ -172,45 +166,17 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
+        'Elastic\\Transport\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/elastic/transport/src',
+        ),
+        'Elastic\\Elasticsearch\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/elasticsearch/elasticsearch/src',
+        ),
         'Dotenv\\' => 
         array (
             0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
-        ),
-        'Doctrine\\Persistence\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/persistence/src/Persistence',
-        ),
-        'Doctrine\\ORM\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/orm/src',
-        ),
-        'Doctrine\\Instantiator\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/instantiator/src/Doctrine/Instantiator',
-        ),
-        'Doctrine\\Inflector\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Inflector',
-        ),
-        'Doctrine\\Deprecations\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/deprecations/lib/Doctrine/Deprecations',
-        ),
-        'Doctrine\\DBAL\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/dbal/src',
-        ),
-        'Doctrine\\Common\\Lexer\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/lexer/src',
-        ),
-        'Doctrine\\Common\\Collections\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/collections/src',
-        ),
-        'Doctrine\\Common\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/event-manager/src',
         ),
         'Aws\\' => 
         array (
@@ -249,7 +215,6 @@ class ComposerStaticInit1087ef01beaabc52553906b3d580865b
         'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
         'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
         'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
-        '©' => __DIR__ . '/..' . '/symfony/cache/Traits/ValueWrapper.php',
     );
 
     public static function getInitializer(ClassLoader $loader)

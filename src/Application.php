3057b8b04ed9805a46e7c60fa3957be327893b82
Leonov <slatch@yandex.ru>
2024-05-27 19:09:08 +0200

    [CORE-7593] [Pdffiller] Analyze "S3 Inventory" logs

--- src/Application.php
+++ src/Application.php
@@ -225,9 +225,12 @@ final class Application
 }
 
 /**
-docker run -e SERVICE_NAME="st-parser" -e AWS_ACCESS_KEY_ID="" -e AWS_SECRET_ACCESS_KEY="" -e ARN_ROLE_READ="arn:aws:iam::811130481316:role/ppf-st-parser-s3-role" -e BUCKET_NAME_READ="ppf-logs-20190701122158291000000001" -e REGION_READ="us-east-1" -e STATISTIC_FOLDER_PATH="fs_s3_statistic/ppf-fileservice-20180927091328208800000001/fs_s3_inventory/"  -it --name parser-2024-03-05 --rm statistic-aggregator:v2 php app.php --dates=2024-03-05
+docker run -e SERVICE_NAME="st-parser" -e AWS_ACCESS_KEY_ID="" -e AWS_SECRET_ACCESS_KEY="" -e ARN_ROLE_READ="arn:aws:iam::811130481316:role/ppf-st-parser-s3-role" -e BUCKET_NAME_READ="ppf-logs-20190701122158291000000001" -e REGION_READ="us-east-1" -e STATISTIC_FOLDER_PATH="fs_s3_statistic/ppf-fileservice-20180927091328208800000001/fs_s3_inventory/" -d -it --name parser-2024-03-05 --rm statistic-aggregator:v2 php app.php --dates=2024-03-05
 
 ssh -A andrii.leonov@bastion-v2.pdffiller.com -i ~/.ssh/id_rsa
 ssh ubuntu@10.20.105.147
 sudo su
+
+docker logs parser-2024-03-05 | tail -5
+
  */
